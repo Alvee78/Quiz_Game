1c253fa610350e3e2219611efd3ab10a7f0ef3f3
Alvee <alvee.rucse@gmail.com>
2023-09-06 20:08:18 +0600

    Question controller

---
+++ QuestionBank.java
@@ -0,0 +1,19 @@
+//package Project.QuizGame;
+import java.util.ArrayList;
+
+public class QuestionBank {
+    private ArrayList<Question> questions;
+
+    public QuestionBank() {
+        questions = new ArrayList<>();
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+}
+
